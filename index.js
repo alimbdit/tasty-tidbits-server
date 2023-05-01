a30ae4dbe172930f4dcd82fef134114183b427d0
alimbdit <alimbdit@gmail.com>
2023-05-02 04:52:45 +0600

    single chef get by id is okey

--- index.js
+++ index.js
@@ -9,6 +9,12 @@ app.get('/', (req, res) => {
     res.send('Chef is running.')
 })
 
+app.get('/chef/:id', (req, res) => {
+    const id = req.params.id;
+    const targetChef = allChef.find(chef => chef.id === id);
+    res.send(targetChef);
+})
+
 app.get('/chef', (req, res) => {
     res.send(allChef)
 })
