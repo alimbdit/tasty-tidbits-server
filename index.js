648d5e781c7c96cd801d4c3ea4f8d8596950620c
alimbdit <alimbdit@gmail.com>
2023-05-02 18:39:44 +0600

    express cors problem fixed

--- index.js
+++ index.js
@@ -2,8 +2,13 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
 
+const cors = require('cors');
+
+
 
 const allChef = require('./data/chef.json');
+
+app.use(cors());
 
 app.get('/', (req, res) => {
     res.send('Chef is running.')
